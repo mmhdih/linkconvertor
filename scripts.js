c210cb57688f7a591383a129c5e53618bbea220d
mmhdih <74430445+mmhdih@users.noreply.github.com>
2022-11-07 21:54:18 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ var newP = document.getElementById("newP");
 function changeValue() {
     textField = document.getElementById("textField1");
     var innerValue = textField.value;
-    var newValue = innerValue.replace("5.75.130.20", "95.217.245.72").replace("116.202.3.144", "95.217.245.72").replace("88.198.122.138", "95.217.245.72");
+    var newValue = innerValue.replace("5.75.130.20", "45.139.11.167").replace("116.202.3.144", "45.139.11.167").replace("88.198.122.138", "45.139.11.167").replace("95.217.245.72", "45.139.11.167");
     newP.innerHTML = newValue;
     newP.value = newValue;
 
