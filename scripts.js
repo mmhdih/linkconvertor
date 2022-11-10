9da0d1db147c25fee9b8c60928c2a5ff8808cfca
mmhdih <74430445+mmhdih@users.noreply.github.com>
2022-11-10 11:55:38 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ var newP = document.getElementById("newP");
 function changeValue() {
     textField = document.getElementById("textField1");
     var innerValue = textField.value;
-    var newValue = innerValue.replace("5.75.130.20", "45.139.11.167").replace("116.202.3.144", "45.139.11.167").replace("88.198.122.138", "45.139.11.167").replace("95.217.245.72", "45.139.11.167").replace("bat1.tigerkala.com", "45.139.11.167").replace("65.108.48.150", "103.215.221.228");
+//    var newValue = innerValue.replace("5.75.130.20", "45.139.11.167").replace("116.202.3.144", "45.139.11.167").replace("88.198.122.138", "45.139.11.167").replace("95.217.245.72", "45.139.11.167").replace("bat1.tigerkala.com", "45.139.11.167").replace("65.108.48.150", "103.215.221.228");
+    var newValue = innerValue.replace("65.108.48.150", "103.215.221.228");
     newP.innerHTML = newValue;
     newP.value = newValue;
 
